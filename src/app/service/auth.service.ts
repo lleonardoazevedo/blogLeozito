5609f56ec89006f7328078c3b776a137cd13302c
Leonardo <lleonardo.azevedo@gmail.com>
2021-11-10 16:57:38 -0300

    [inicio e menu alterado]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -15,10 +16,19 @@ export class AuthService {
 
   entrar(userLogin: UserLogin):Observable<UserLogin>{
     return this.http.post<UserLogin>('https://blogdoleo.herokuapp.com/api/v1/usuarios/logar', userLogin)
-
   }
+
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogdoleo.herokuapp.com/api/v1/usuarios/cadastrar', usuario)
+  }
 
+  logado(){
+    let ok = false
+    
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
